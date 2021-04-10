e6b611fbb4032aad6f606230adf3542d684d6ad9
Frank E. Hernandez <hernandez.frank@gmail.com>
2021-04-10 13:34:43 -0400

    Added Sample for RJ printer.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final controller = PageController (
+    initialPage: 1
+  );
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -29,13 +33,15 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PageView(children: [
+        BleRjPrintPage(title: 'RJ-4250WB BLE Sample'),
+        WifiPrintPage(title: 'QL-1110 WiFi Sample')]),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class WifiPrintPage extends StatefulWidget {
+  WifiPrintPage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -49,10 +55,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _WifiPrintPageState createState() => _WifiPrintPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _WifiPrintPageState extends State<WifiPrintPage> {
 
   bool _error = false;
 
@@ -76,7 +82,10 @@ class _MyHomePageState extends State<MyHomePage> {
     if (printers.isEmpty) {
       // Show a message if no printers are found.
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("No printers found on your network."),
+        content: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("No printers found on your network."),
+        ),
       ));
 
       return;
@@ -108,23 +117,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-              Image(image: AssetImage('assets/brother_hack.png'))
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Don't forget to grant permissions to your app in Settings.", textAlign: TextAlign.center,),
+            ),
+            Image(image: AssetImage('assets/brother_hack.png'))
           ],
         ),
       ),
@@ -144,16 +143,120 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     return completer.future;
   }
+}
 
-  /*
-  /// Load [Image] from asset path.
-  /// https://stackoverflow.com/a/61338308/1321917
-  Future<Image> loadUiImage(String assetPath) async {
-    final data = await rootBundle.load(assetPath);
-    final list = Uint8List.view(data.buffer);
-    final completer = Completer<Image>();
-    ui.decodeImageFromList(list, completer.complete);
+
+class BleRjPrintPage extends StatefulWidget {
+  BleRjPrintPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _BleRjPrintPageState createState() => _BleRjPrintPageState();
+}
+
+class _BleRjPrintPageState extends State<BleRjPrintPage> {
+
+  bool _error = false;
+
+  void print(BuildContext context) async {
+
+    var printer = new Printer();
+    var printInfo = PrinterInfo();
+    printInfo.printerModel = Model.RJ_4250WB;
+    printInfo.printMode = PrintMode.FIT_TO_PAGE;
+    printInfo.isAutoCut = true;
+    printInfo.port = Port.BLE;
+    // Set the label type.
+    double width = 102.0;
+    double rightMargin = 0.0;
+    double leftMargin = 0.0;
+    double topMargin = 0.0;
+    CustomPaperInfo customPaperInfo = CustomPaperInfo.newCustomRollPaper(printInfo.printerModel,
+        Unit.Mm,
+        width,
+        rightMargin,
+        leftMargin,
+        topMargin);
+    printInfo.customPaperInfo = customPaperInfo;
+
+    // Set the printer info so we can use the SDK to get the printers.
+    await printer.setPrinterInfo(printInfo);
+
+    // Get a list of printers with my model available in the network.
+    List<BLEPrinter> printers = await printer.getBLEPrinters(3000);
+
+    if (printers.isEmpty) {
+      // Show a message if no printers are found.
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("No printers found on your network."),
+        ),
+      ));
+
+      return;
+    }
+    // Get the BT name from the first printer found.
+    printInfo.setLocalName(printers.single.localName);
+
+    printer.setPrinterInfo(printInfo);
+    printer.printImage(await loadImage('assets/brother_hack.png'));
+
+  }
+  @override
+  Widget build(BuildContext context) {
+
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Don't forget to grant permissions to your app in Settings.", textAlign: TextAlign.center,),
+            ),
+            Image(image: AssetImage('assets/brother_hack.png'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print(context),
+        tooltip: 'Print',
+        child: Icon(Icons.print),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<ui.Image> loadImage(String assetPath) async {
+    final ByteData img = await rootBundle.load(assetPath);
+    final Completer<ui.Image> completer = new Completer();
+    ui.decodeImageFromList(new Uint8List.view(img.buffer), (ui.Image img) {
+      return completer.complete(img);
+    });
     return completer.future;
   }
-   */
 }
