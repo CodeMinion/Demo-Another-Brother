9413aa3e991250209fd5e54cecb119ce6ae36300
Frank E. Hernandez <hernandez.frank@gmail.com>
2021-04-19 22:25:06 -0400

    Updated to include Wifi Sample with PJ-773

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,9 @@ class MyApp extends StatelessWidget {
       ),
       home: PageView(children: [
         BleRjPrintPage(title: 'RJ-4250WB BLE Sample'),
-        WifiPrintPage(title: 'QL-1110 WiFi Sample')]),
+        QlBluetoothPrintPage(title: 'QL-1110NWB Bluetooth Sample'),
+        WifiPrintPage(title: 'PJ-773 WiFi Sample')]),
+
     );
   }
 }
@@ -66,18 +68,18 @@ class _WifiPrintPageState extends State<WifiPrintPage> {
 
     var printer = new Printer();
     var printInfo = PrinterInfo();
-    printInfo.printerModel = Model.QL_1110NWB;
+    printInfo.printerModel = Model.PJ_773;
     printInfo.printMode = PrintMode.FIT_TO_PAGE;
     printInfo.isAutoCut = true;
     printInfo.port = Port.NET;
     // Set the label type.
-    printInfo.labelNameIndex = QL1100.ordinalFromID(QL1100.W103.getId());
+    printInfo.paperSize = PaperSize.A4;
 
     // Set the printer info so we can use the SDK to get the printers.
     await printer.setPrinterInfo(printInfo);
 
     // Get a list of printers with my model available in the network.
-    List<NetPrinter> printers = await printer.getNetPrinters([Model.QL_1110NWB.getName()]);
+    List<NetPrinter> printers = await printer.getNetPrinters([Model.PJ_773.getName()]);
 
     if (printers.isEmpty) {
       // Show a message if no printers are found.
@@ -208,6 +210,111 @@ class _BleRjPrintPageState extends State<BleRjPrintPage> {
     }
     // Get the BT name from the first printer found.
     printInfo.setLocalName(printers.single.localName);
+
+    printer.setPrinterInfo(printInfo);
+    printer.printImage(await loadImage('assets/brother_hack.png'));
+
+  }
+  @override
+  Widget build(BuildContext context) {
+
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Don't forget to grant permissions to your app in Settings.", textAlign: TextAlign.center,),
+            ),
+            Image(image: AssetImage('assets/brother_hack.png'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print(context),
+        tooltip: 'Print',
+        child: Icon(Icons.print),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<ui.Image> loadImage(String assetPath) async {
+    final ByteData img = await rootBundle.load(assetPath);
+    final Completer<ui.Image> completer = new Completer();
+    ui.decodeImageFromList(new Uint8List.view(img.buffer), (ui.Image img) {
+      return completer.complete(img);
+    });
+    return completer.future;
+  }
+}
+
+class QlBluetoothPrintPage extends StatefulWidget {
+  QlBluetoothPrintPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _QlBluetoothPrintPageState createState() => _QlBluetoothPrintPageState();
+}
+
+class _QlBluetoothPrintPageState extends State<QlBluetoothPrintPage> {
+
+  bool _error = false;
+
+  void print(BuildContext context) async {
+
+    var printer = new Printer();
+    var printInfo = PrinterInfo();
+    printInfo.printerModel = Model.QL_1110NWB;
+    printInfo.printMode = PrintMode.FIT_TO_PAGE;
+    printInfo.isAutoCut = true;
+    printInfo.port = Port.BLUETOOTH;
+    // Set the label type.
+    printInfo.labelNameIndex = QL1100.ordinalFromID(QL1100.W103.getId());
+
+    // Set the printer info so we can use the SDK to get the printers.
+    await printer.setPrinterInfo(printInfo);
+
+    // Get a list of printers with my model available in the network.
+    List<BluetoothPrinter> printers = await printer.getBluetoothPrinters([Model.QL_1110NWB.getName()]);
+
+    if (printers.isEmpty) {
+      // Show a message if no printers are found.
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("No paired printers found on your device."),
+        ),
+      ));
+
+      return;
+    }
+    // Get the IP Address from the first printer found.
+    printInfo.macAddress = printers.single.macAddress;
 
     printer.setPrinterInfo(printInfo);
     printer.printImage(await loadImage('assets/brother_hack.png'));
